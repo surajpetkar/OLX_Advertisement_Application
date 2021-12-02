51eb11d0ceadc1d9faedece12270ad8f5f620098
surajpetkar <80508224+surajpetkar@users.noreply.github.com>
2021-12-02 18:48:34 +0530

    Update LoginRepository.java

--- OLX-Login/src/main/java/com/zensar/repository/LoginRepository.java
+++ OLX-Login/src/main/java/com/zensar/repository/LoginRepository.java
@@ -1,10 +1,10 @@
 package com.zensar.repository;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.zensar.model.User;
 
-public interface LoginRepository extends MongoRepository<User,String>{
+public interface LoginRepository extends JpaRepository<User,String>{
 
 	public User findByUserName(String username);
 }
