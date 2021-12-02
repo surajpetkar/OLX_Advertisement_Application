218f897b7f344f1d5eb47021691b1c27610c23d9
Suraj Petkar <suraj.petkar@zensar.com>
2021-12-02 18:28:31 +0530

    API-Gateway

---
+++ OLX-API-Gateway/src/main/java/com/zensar/OlxApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.zensar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlxApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlxApiGatewayApplication.class, args);
+	}
+
+}
